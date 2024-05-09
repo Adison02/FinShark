c5f3521287521074576de8249b5f7d3deba0714c
Adrian Mazur <adrianmazur0660@gmail.com>
2024-05-09 22:27:34 +0200

    Pagination

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1479824678ec214590058fbeb7ddbe52252c0b35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dea4a91724e992462a616b4d417d08d947eadc7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
