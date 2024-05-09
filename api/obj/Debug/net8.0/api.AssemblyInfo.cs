d04d9019e8cd7e8df8fa04ab236fa8eed47cc314
Adrian Mazur <adrianmazur0660@gmail.com>
2024-05-09 22:47:21 +0200

    JWT Install Identity

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dea4a91724e992462a616b4d417d08d947eadc7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5f3521287521074576de8249b5f7d3deba0714c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
