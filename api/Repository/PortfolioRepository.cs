2054dcc6dfb6785ed377c71aa8601150fa6ec354
Adrian Mazur <adrianmazur0660@gmail.com>
2024-05-11 19:35:37 +0200

    Portfolio GET

---
+++ api/Repository/PortfolioRepository.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository
+{
+    public class PortfolioRepository : IPortfolioRepository
+    {
+        private readonly ApplicationDBContext _context;
+
+        public PortfolioRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public async  Task<List<Stock>> GetUserPortfolio(AppUser user)
+        {
+            return await _context.Portfolios.Where(u => u.AppUserId == user.Id)
+                .Select(stock => new Stock
+                {
+                    Id = stock.StockId,
+                    CompanyName = stock.Stock.CompanyName,
+                    Symbol = stock.Stock.Symbol,
+                    Industry = stock.Stock.Industry,
+                    Purchase = stock.Stock.Purchase,
+                    LastDiv = stock.Stock.LastDiv,
+                    MarketCap = stock.Stock.MarketCap
+                }).ToListAsync();
+        }
+    }
+}
