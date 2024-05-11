be09a6b49e552ff201c175b73344639ace98e849
Adrian Mazur <adrianmazur0660@gmail.com>
2024-05-11 19:00:45 +0200

    JWT Login

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using api.Models;
 using api.Service;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -18,11 +19,39 @@ namespace api.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _singInManager;
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> singInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _singInManager = singInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(u => u.UserName == loginDto.Username);
+
+            if(user == null)
+                return Unauthorized("Invalid username!");
+
+            var result = await _singInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if(!result.Succeeded)
+                return Unauthorized("Username not found and/or password incorrect.");
+            
+            return Ok(
+                new NewUserDto
+                {
+                    Username = user.UserName,
+                    Email = user.Email,
+                    Token = _tokenService.CreateToken(user)
+                }
+            );
         }
 
         [HttpPost("register")]
