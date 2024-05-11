c5c308e226a2b4d27c442da38b9dafe771343109
Adrian Mazur <adrianmazur0660@gmail.com>
2024-05-11 21:29:01 +0200

    Comment Query

--- api/Repository/CommentRepository.cs
+++ api/Repository/CommentRepository.cs
@@ -6,7 +6,9 @@ using api.Data;
 using api.Dtos.Comment;
 using api.Interfaces;
 using api.Models;
+using api.Helpers;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.SymbolStore;
 
 namespace api.Repository
 {
@@ -19,9 +21,21 @@ namespace api.Repository
             _context = context;
         }
 
-        public async Task<List<Comment>> GetAllAsync()
+        public async Task<List<Comment>> GetAllAsync(CommentQueryObject queryObject)
         {
-            return await _context.Comments.Include(a => a.AppUser).ToListAsync();
+            var comments = _context.Comments.Include(a => a.AppUser).AsQueryable();
+
+            if(!string.IsNullOrWhiteSpace(queryObject.Symbol))
+            {
+                comments = comments.Where(s => s.Stock.Symbol == queryObject.Symbol);
+            }
+
+            if(queryObject.IsDescending == true)
+            {
+                comments = comments.OrderByDescending(c => c.Created);
+            }
+
+            return await comments.ToListAsync();
         }
 
         public async Task<Comment?> GetByIdAsync(int id)
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22099575ae59609c630acf8dc8173c25fd531f07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db43f8158e6091bbfcb79ce618c23007dca9f8f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
